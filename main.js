aae7fe7cf70ab6d95f0cb078e5f7e455ffe73cfe
Emma <jsj0471@naver.com>
2021-09-26 17:16:38 +0900

    JS : make random puzzle

--- main.js
+++ main.js
@@ -5,14 +5,34 @@ const gamePuzzle = document.querySelector('.game__puzzle');
 const gameStartBtn = document.querySelector('.game__start--button');
 const gameText = document.querySelector('.game__text');
 
-let puzzleCount = 16;
+let tileCount = 16;
+let tiles = [];
+tiles = creatImageTiles();
+
+shuffle(tiles).forEach(tile => gamePuzzle.appendChild(tile));
 
  // make puzzle in Javascript
- Array(puzzleCount).fill().forEach((_,index) => {
-     const puzzlePiece = document.createElement('li');
-     puzzlePiece.setAttribute('data-index', index);
-     puzzlePiece.classList.add(`list${index}`);
+function creatImageTiles(){
+    const tempArray = [];
+         
+    Array(tileCount).fill().forEach((_,index) => {
+        const li = document.createElement('li');
+         li.setAttribute('data-index', index);
+         li.classList.add(`list${index}`);
+        
+         tempArray.push(li);
+    });
+    return tempArray;
+}
 
-    gamePuzzle.appendChild(puzzlePiece);
-});
+// make random puzzle
+function shuffle(array) {
+    let index = array.length - 1;
+    while(index > 0){
+        const randomIndex = Math.floor(Math.random()*(index+1));
+        [array[index],array[randomIndex]] = [array[randomIndex], array[index]];
+        --index;
+    }
+    return array;
+}
 

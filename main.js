9990ed5c6eeb6931a9126fc507cfe494775f1d56
Emma <jsj0471@naver.com>
2021-09-26 16:00:19 +0900

    CSS : image position

---
+++ main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const gameTimer = document.querySelector('game__timer');
+const gamePuzzle = document.querySelector('game__puzzle');
+const gameStartBtn = document.querySelector('game__start--button');
+const gameText = document.querySelector('game__text');
+
+let puzzleCount = 16;
+
+// make puzzle in Javascript
+// Array(puzzleCount).fill().forEach((_,index) => {
+//     const puzzlePiece = document.createElement('li')    
+// });
+

20cdb6999f6bf7a057ae620417cca0df77e180e6
Emma <jsj0471@naver.com>
2021-09-26 16:26:29 +0900

    JS : make lists in javascript

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 'use strict';
 
-const gameTimer = document.querySelector('game__timer');
-const gamePuzzle = document.querySelector('game__puzzle');
-const gameStartBtn = document.querySelector('game__start--button');
-const gameText = document.querySelector('game__text');
+const gameTimer = document.querySelector('.game__timer');
+const gamePuzzle = document.querySelector('.game__puzzle');
+const gameStartBtn = document.querySelector('.game__start--button');
+const gameText = document.querySelector('.game__text');
 
 let puzzleCount = 16;
 
-// make puzzle in Javascript
-// Array(puzzleCount).fill().forEach((_,index) => {
-//     const puzzlePiece = document.createElement('li')    
-// });
+ // make puzzle in Javascript
+ Array(puzzleCount).fill().forEach((_,index) => {
+     const puzzlePiece = document.createElement('li');
+     puzzlePiece.setAttribute('data-index', index);
+     puzzlePiece.classList.add(`list${index}`);
+
+    gamePuzzle.appendChild(puzzlePiece);
+});
 
